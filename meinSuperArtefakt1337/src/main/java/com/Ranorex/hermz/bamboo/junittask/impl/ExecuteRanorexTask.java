f9b38840362254f27669ba6487b227ee237212a0
hermz <hermzhafner@gmail.com>
2017-11-15 16:20:36 +0100

    initial.

--- meinSuperArtefakt1337/src/main/java/com/Ranorex/hermz/bamboo/junittask/impl/ExecuteRanorexTask.java
+++ meinSuperArtefakt1337/src/main/java/com/Ranorex/hermz/bamboo/junittask/impl/ExecuteRanorexTask.java
@@ -17,17 +17,26 @@ import com.atlassian.bamboo.task.TaskType;
 import edu.emory.mathcs.backport.java.util.Arrays;
 
 public class ExecuteRanorexTask implements TaskType {
+	private static final String _FIXED_JUNIT_XML = "_fixed.junit.xml";
+	private static final String JUNIT_XML = ".junit.xml";
+	private static final String TEST_EXECUTED_RESULT = "Test executed. Result: ";
+	private static final String REPORT_RXLOG = "report.rxlog";
+	private static final String EXE_RF = ".exe /rf:";
+	private static final String DEBUG = "Debug";
+	private static final String BIN = "bin";
+	private static final String RXSLN = ".rxsln";
+	private static final String END_RANOREX_TEST = "End Ranorex Test";
+	private static final String START_RANOREX_TEST = "Start Ranorex Test";
+	static String slName = "";
 
 	@SuppressWarnings("deprecation")
 	public TaskResult execute(TaskContext taskContext) throws TaskException {
 		final BuildLogger buildLogger = taskContext.getBuildLogger();
-
-		buildLogger.addBuildLogEntry("Start Ranorex Test");
-
 		int result = -1;
+		buildLogger.addBuildLogEntry(START_RANOREX_TEST);
+
 		try {
 			result = executeTest(taskContext);
-
 			if (result == 0) {
 				return TaskResultBuilder.create(taskContext).success().build();
 			} else {
@@ -40,61 +49,63 @@ public class ExecuteRanorexTask implements TaskType {
 			buildLogger.addBuildLogEntry(e.getMessage());
 			return TaskResultBuilder.create(taskContext).failedWithError().build();
 		} finally {
-			buildLogger.addBuildLogEntry("End Ranorex Test");
+			buildLogger.addBuildLogEntry(END_RANOREX_TEST);
 		}
 	}
-
-	static String slName = "";
 
 	private synchronized int executeTest(TaskContext taskContext) throws IOException, InterruptedException {
 		taskContext.getBuildLogger().addBuildLogEntry(taskContext.getRootDirectory().getAbsolutePath());
 		File f = taskContext.getRootDirectory();
-
-		for (File file : f.listFiles()) {
-			if (file.getName().endsWith(".rxsln")) {
-				slName = file.getName().replaceAll(".rxsln", "");
-			}
-		}
-		
-				
-		String execFile = f.getAbsolutePath() + "\\" + slName + "\\bin" + "\\Debug\\" + slName + ".exe /rf:" + taskContext.getRootDirectory() + "\\report.rxlog";
 		File jFile = null;
 		int result = -1;
-
-//		Process process = new ProcessBuilder(execFile).start();
+		String execFile = extracted(taskContext, f);
 		Runtime rt = Runtime.getRuntime();
 		Process process = rt.exec(execFile);
-		
+
 		while (process.isAlive()) {
 			this.wait(1000);
 		}
 		result = process.exitValue();
+		taskContext.getBuildLogger().addBuildLogEntry(TEST_EXECUTED_RESULT + result);
+		jFile = extracted(f, jFile);
+		extracted(jFile);
 
-		taskContext.getBuildLogger().addBuildLogEntry("Test executed. Result: " + result);
-		
-//		File debugfolder = new File(f.getAbsolutePath() + "\\" + slName + "\\bin" + "\\Debug");
+		return result;
+	}
 
+	private String extracted(TaskContext taskContext, File f) {
+		return f.getAbsolutePath() + File.separator + slName + File.separator + BIN + File.separator + DEBUG
+				+ File.separator + slName + EXE_RF + taskContext.getRootDirectory() + File.separator + REPORT_RXLOG;
+	}
+
+	private File extracted(File f, File jFile) {
+		for (File file : f.listFiles()) {
+			if (file.getName().endsWith(RXSLN)) {
+				slName = file.getName().replaceAll(RXSLN, "");
+			}
+		}
 		for (File currentfile : f.listFiles()) {
-			if (currentfile.getName().contains(".junit.xml")) {
+			if (currentfile.getName().contains(JUNIT_XML)) {
 				jFile = currentfile;
 			}
 		}
+		return jFile;
+	}
 
+	private void extracted(File jFile) throws IOException, InterruptedException {
 		if (jFile != null) {
 			while (Files.readAllBytes(jFile.toPath()).length == 3) {
 				this.wait(5000);
 			}
 			byte[] b = Files.readAllBytes(jFile.toPath());
 			byte[] newB = new byte[b.length - 3];
-			
+
 			for (int i = 0; i < newB.length; i++) {
 				newB[i] = b[i + 3];
 			}
-			
-			Path p = Paths.get(jFile.getAbsolutePath().replaceAll(".junit.xml", "_fixed.junit.xml"));
+
+			Path p = Paths.get(jFile.getAbsolutePath().replaceAll(JUNIT_XML, _FIXED_JUNIT_XML));
 			Files.write(p, newB);
 		}
-
-		return result;
 	}
 }
